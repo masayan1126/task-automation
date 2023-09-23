0ee9507807dc95c37fdb57999b5444d0277d3d11
masayan1126 <masa199311266@gmail.com>
2023-09-23 15:09:43 +0900

    First commit

---
+++ content_share/main.py
@@ -0,0 +1,16 @@
+import tweepy
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+client = tweepy.Client(
+    consumer_key=os.getenv("CONSUMER_KEY"),
+    consumer_secret=os.getenv("CONSUMER_SECRET"),
+    access_token=os.getenv("ACCESS_TOKEN"),
+    access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"),
+)
+
+client.create_tweet(text="テストツイート文")
