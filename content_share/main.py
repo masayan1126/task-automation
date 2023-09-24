1d32b5201311bdc8d94bb35ea5d466084985d699
masayan1126 <masa199311266@gmail.com>
2023-09-24 11:11:39 +0900

    upd

--- content_share/main.py
+++ content_share/main.py
@@ -1,30 +1,34 @@
 import tweepy
 from dotenv import load_dotenv
 import os
-
-# import functions_framework
 import base64
 
+"""Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
 
-# @functions_framework.http
+
 def main(event, context):
     load_dotenv()
+    # TODO: cloud schedulerからシェアするコンテンツの情報をjsonなどで受け取る
+    __do_tweet()
 
-    client = tweepy.Client(
+    pubsub_message = base64.b64decode(event["data"]).decode("utf-8")
+    print(pubsub_message)
+    return "Hello {}!!Q".format("name")
+
+
+def __create_client() -> tweepy.Client:
+    return tweepy.Client(
         consumer_key=os.getenv("CONSUMER_KEY"),
         consumer_secret=os.getenv("CONSUMER_SECRET"),
         access_token=os.getenv("ACCESS_TOKEN"),
         access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"),
     )
 
+
+def __do_tweet() -> None:
+    client = __create_client()
     client.create_tweet(text="テストツイート文a")
-
-    """Triggered from a message on a Cloud Pub/Sub topic.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
-
-    pubsub_message = base64.b64decode(event["data"]).decode("utf-8")
-    print(pubsub_message)
-    return "Hello {}!!Q".format("name")
