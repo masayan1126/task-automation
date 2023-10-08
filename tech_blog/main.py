7637b0cb05f004469186e449ef886cbf7a33dd71
masayan1126 <masa199311266@gmail.com>
2023-10-08 15:00:04 +0900

    upd

--- tech_blog/main.py
+++ tech_blog/main.py
@@ -2,9 +2,10 @@ import os
 from dotenv import load_dotenv
 from taopypy.modules.x import do_tweet
 from taopypy.modules.notification import notify_to_slack
+import requests
 
 
-def main(event, context):
+def share(event, context):
     try:
         load_dotenv()
 
@@ -50,6 +51,47 @@ def main(event, context):
         # pubsub_message = base64.b64decode(event["data"]).decode("utf-8")
         # print(pubsub_message)
         # print(f"context is {context}")
+
+    except Exception as e:
+        # e_type, e_value, e_traceback = sys.exc_info()
+        print(e)
+        # logging.error("Exception type : %s " % e_type.__name__)
+        # logging.error("Exception message : %s " % e_value.__name__)
+        # logging.error("Stack trace : %s " % e_traceback.__name__)
+
+        raise e
+
+
+def collect(event, context):
+    try:
+        load_dotenv()
+
+        articles = []
+
+        ENDPOINT = "https://quxwm5ub3d.microcms.io/api/v1/blogs?limit=10&offset=0"
+
+        X_MICROCMS_API_KEY = os.getenv("X_MICROCMS_API_KEY", "")
+
+        headers = {"X-MICROCMS-API-KEY": X_MICROCMS_API_KEY}
+
+        res = requests.get(ENDPOINT, headers=headers)
+        data = res.json()
+
+        for item in data["contents"]:
+            articles.append(
+                {
+                    "url": item["url"],
+                    "title": item["title"],
+                }
+            )
+
+        # 週一で
+        # cloud strageからダウンロード
+        # idの配列を取得し、配列の中にないIDを元の配列に追加して再度アップロード
+
+        print("対象の記事一覧:")
+        for article in articles:
+            print(f"url: {article['url']}, title: {article['title']}")
 
     except Exception as e:
         # e_type, e_value, e_traceback = sys.exc_info()
