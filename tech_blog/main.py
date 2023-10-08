dcbaebc1eb2b7466a242205688917d158e72cf11
masayan1126 <masa199311266@gmail.com>
2023-10-08 16:49:37 +0900

    upd

--- tech_blog/main.py
+++ tech_blog/main.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 from taopypy.modules.x import do_tweet
 from taopypy.modules.notification import notify_to_slack
 import requests
+from google.cloud import storage
+from google.oauth2 import service_account
+import pandas as pd
 
 
 def share(event, context):
@@ -66,32 +69,56 @@ def collect(event, context):
     try:
         load_dotenv()
 
-        articles = []
+        # articles = []
 
-        ENDPOINT = "https://quxwm5ub3d.microcms.io/api/v1/blogs?limit=10&offset=0"
+        # ENDPOINT = "https://quxwm5ub3d.microcms.io/api/v1/blogs?limit=10&offset=0"
 
-        X_MICROCMS_API_KEY = os.getenv("X_MICROCMS_API_KEY", "")
+        # X_MICROCMS_API_KEY = os.getenv("X_MICROCMS_API_KEY", "")
 
-        headers = {"X-MICROCMS-API-KEY": X_MICROCMS_API_KEY}
+        # headers = {"X-MICROCMS-API-KEY": X_MICROCMS_API_KEY}
 
-        res = requests.get(ENDPOINT, headers=headers)
-        data = res.json()
+        # res = requests.get(ENDPOINT, headers=headers)
+        # data = res.json()
 
-        for article in data["contents"]:
-            articles.append(
-                {
-                    "id": article["id"],
-                    "title": article["title"],
-                }
-            )
+        # for article in data["contents"]:
+        #     articles.append(
+        #         {
+        #             "id": article["id"],
+        #             "title": article["title"],
+        #         }
+        #     )
+
+        credentials = service_account.Credentials.from_service_account_file(
+            os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE", "")
+        )
+
+        scoped_credentials = credentials.with_scopes(
+            [
+                "https://www.googleapis.com/auth/cloud-platform",
+            ]
+        )
+
+        client = storage.Client(
+            credentials=scoped_credentials,
+            project=scoped_credentials.project_id,
+        )
+
+        bucket = client.get_bucket("tech-blog-articles")
+
+        # 格納するGCSパスを指定
+        blob = bucket.blob("")
+
+        df = pd.DataFrame([[1, 2, 3], [5, 6, 7]], columns=["x", "y", "z"])
+        # DataFrameをGCSにUpload(ヘッダーは入れるが、インデックスは落とす)
+        blob.upload_from_string(df.to_csv(sep=","))
 
         # 週一で
         # cloud strageからダウンロード
         # idの配列を取得し、配列の中にないIDを元の配列に追加して再度アップロード
 
-        print("対象の記事一覧:")
-        for article in articles:
-            print(f"id: {article['id']}, title: {article['title']}")
+        # print("対象の記事一覧:")
+        # for article in articles:
+        #     print(f"id: {article['id']}, title: {article['title']}")
 
     except Exception as e:
         # e_type, e_value, e_traceback = sys.exc_info()
