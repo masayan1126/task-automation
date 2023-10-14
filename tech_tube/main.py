e07fb32341594090e5029bbce0f31de8745e0c1b
masayan1126 <masa199311266@gmail.com>
2023-10-14 14:47:37 +0900

    update

--- tech_tube/main.py
+++ tech_tube/main.py
@@ -46,10 +46,4 @@ def main(event, context):
         # print(f"context is {context}")
 
     except Exception as e:
-        # e_type, e_value, e_traceback = sys.exc_info()
         print(e)
-        # logging.error("Exception type : %s " % e_type.__name__)
-        # logging.error("Exception message : %s " % e_value.__name__)
-        # logging.error("Stack trace : %s " % e_traceback.__name__)
-
-        raise e
