dfb673d7079ddd3909ec78185503b7e187112aaf
masayan1126 <masa199311266@gmail.com>
2023-10-14 15:47:58 +0900

    upd

--- tech_blog/main.py
+++ tech_blog/main.py
@@ -5,8 +5,10 @@ from features.article import (
     get_articles_from_mcs,
     update_article_info_list,
 )
+from features.score import calc_performance_score
 from taopypy.modules.x import do_tweet
 from taopypy.modules.notification import notify_to_slack
+from taopypy.modules.formatter.error_formatter import ErrorFormatter
 
 
 # python -c "from tech_blog import main; main.share()"
@@ -54,5 +56,32 @@ def collect(event=None, context=None):
         print(e)
 
 
+# 　TODO：これは、pub subではなく、httpで叩く必要があるかも(vercelでビルドデプロイ完了後に叩く)
+def calc_score(event=None, context=None):
+    try:
+        load_dotenv()
+
+        site_url = "https://maasaablog.com/page/1"
+        endpoint = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed"
+
+        strategy_param = "mobile"  # (mobile or desktop)
+        payload = {"strategy": strategy_param, "api_key": os.getenv("GOOGLE_API_KEY")}
+
+        urlName = endpoint + "?url=" + site_url
+        # 測定回数
+        measurement_count = 3
+
+        score_average, score_max, score_min = calc_performance_score(
+            payload, urlName, measurement_count
+        )
+        print(f"\n平均 {score_average} 点（最低 {score_min} 点、最高 {score_max} 点）")
+
+    except Exception as e:
+        print(e)
+        formatted = ErrorFormatter().format(e)
+        print(formatted["message"])
+        print(formatted["stack_trace"])
+
+
 if __name__ == "__main__":
-    share()
+    calc_score()
