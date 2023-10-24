25935038254e5bff909ceb18c2120d9476813ce6
masayan1126 <masa199311266@gmail.com>
2023-10-24 20:18:45 +0900

    upd

--- tech_blog_article_share_service/main.py
+++ tech_blog_article_share_service/main.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
+
+
+
 # 参考：https://zenn.dev/eito_blog/articles/94dc874c112c9f
 def main(event=None, context=None):
-    print("share_video_list")
+    load_dotenv()
+
+
